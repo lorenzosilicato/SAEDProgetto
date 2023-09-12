2a1c3c5a07ac0e99a7a9f52901fc7943d2eb6082
Lorenzo <lollo.silicato@gmail.com>
2023-09-12 11:27:48 +0200

    First commit

--- config/auth.js
+++ config/auth.js
@@ -5,5 +5,13 @@ module.exports = {
         }
         req.flash('error_msg', 'Serve essere loggati per accedere a questa pagina');
         res.redirect('/users/login');
+    },
+
+    isAdmin : function (req, res, next) {
+        if (req.user.ruolo == 'Admin'){
+            return next();
+        }
+        req.flash('error_msg', 'Questa pagina è riservata agli amministratori!');
+        res.redirect('/dashboard');
     }
 }
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {ensureAuthenticated} = require("../config/auth");
+const {ensureAuthenticated, isAdmin} = require("../config/auth");
 const router = express.Router();
 const pool = require('../db');
 
@@ -28,7 +28,9 @@ router.get('/dashboard', ensureAuthenticated, (req, res)=>{
     });
 });
 
+//Handler Dashboard
 router.post('/dashboard', function (req, res){
+    let errors= [];
     let id_prenotazione = req.body.id_prenotazione;
     pool.query('SELECT id_prenotazione, tipo_prenotazione FROM salalan.prenotazioni WHERE id_prenotazione=$1',[id_prenotazione], function (error, results) {
         if (error) {
@@ -60,8 +62,117 @@ router.post('/dashboard', function (req, res){
                 res.redirect('../dashboard');
             })
         }
-    })
+    });
 });
 
+
+
+
+//Sezione Admin
+
+
+router.get('/adminDashboard',ensureAuthenticated, isAdmin, (req, res)=>{
+    let errors= [];
+    const text = "SELECT prenotazioni.id_prenotazione, prenotazioni.data, prenotazioni.ora_inizio, prenotazioni.ora_fine, tipo_prenotazione,gioco, prenotazioni.postazione FROM salalan.prenotazioni LEFT JOIN salalan.iscrizione ON prenotazioni.id_prenotazione=iscrizione.id_prenotazione LEFT JOIN salalan.torneo ON iscrizione.id_torneo=torneo.id_torneo LEFT JOIN salalan.utente ON iscrizione.id_utente=utente.nome ORDER BY data;";
+    pool.query(text, function (error, results){
+        if(error){
+            errors.push({msg : "Errore di selezione interno al database, contatta il tecnico se persiste"});
+            res.redirect('/homepage', {
+                errors
+            });
+        }
+        let datiPrenotazioniAdmin = results.rows;
+        pool.query("SELECT * FROM salalan.torneo", (error, results) => {
+            if(error) {
+                errors.push({msg: "Errore di selezione interno al database, contatta il tecnico se persiste"});
+                res.redirect('/homepage', {
+                    errors
+                });
+            }
+            res.render('adminDashboard', {
+                nome : req.user.nome,
+                email : req.user.email,
+                numero_telefono : req.user.numero_telefono,
+                datiPrenotazioniAdmin : datiPrenotazioniAdmin,
+                datiTornei : results.rows
+            });
+
+        });
+    });
+});
+
+router.post('/adminDashboard/cancellaPrenotazione', (req, res)=>{
+    let errors= [];
+    let id_prenotazione = req.body.id_prenotazione;
+    pool.query('SELECT id_prenotazione, tipo_prenotazione FROM salalan.prenotazioni WHERE id_prenotazione=$1',[id_prenotazione], function (error, results) {
+        if (error) {
+            errors.push({msg : "Errore di selezione interno al database, contatta il tecnico se persiste"});
+            res.render('/homepage', {
+                errors
+            });
+        }
+        if(results.rows[0].tipo_prenotazione === 'Torneo'){
+            pool.query('DELETE FROM salalan.iscrizione WHERE iscrizione.id_prenotazione =$1', [id_prenotazione], function (error, risposta) {
+                if (error) {
+                    errors.push({msg : "Errore di cancellazione interno al database, contatta il tecnico se persiste"});
+                    res.render('/dashboard', {
+                        errors
+                    });
+                }
+                req.flash('success_msg', 'Cancellazione andata a buon fine!');
+                res.redirect('../dashboard');
+            })
+        } else {
+            pool.query('DELETE FROM salalan.prenotazioni WHERE prenotazioni.id_prenotazione =$1', [id_prenotazione], function (error, risposta) {
+                if (error){
+                    errors.push({msg : "Errore di cancellazione interno al database, contatta il tecnico se persiste"});
+                    res.render('/dashboard', {
+                        errors
+                    });
+                }
+                req.flash('success_msg', 'Cancellazione andata a buon fine!');
+                res.redirect('../adminDashboard');
+            })
+        }
+    });
+});
+
+router.post('/adminDashboard/cancellaTorneo', (req, res)=>{
+    let errors= [];
+    let id_torneo = req.body.id_torneo;
+    pool.query('DELETE FROM salalan.iscrizione WHERE iscrizione.id_torneo = $1', [id_torneo], function (error, results){
+        if(error){
+            errors.push({msg : "Errore di cancellazione interno al database, contatta il tecnico se persiste"});
+            res.render('/dashboard', {
+                errors
+            });
+        }
+        pool.query('DELETE FROM salalan.torneo WHERE id_torneo = $1', [id_torneo], function (error, results) {
+            if (error){
+                errors.push({msg : "Errore di cancellazione interno al database, contatta il tecnico se persiste"});
+                res.render('/dashboard', {
+                    errors
+                });
+            }
+            req.flash('success_msg', 'Cancellazione andata a buon fine!');
+            res.redirect('../adminDashboard');
+        });
+    });
+});
+
+router.post('/adminDashboard/creaTorneo', (req, res)=>{
+    let errors= [];
+    const {gioco, data, ora_inizio, ora_fine, max_iscritti} = req.body;
+    pool.query('INSERT INTO salalan.torneo(gioco,data,ora_inizio, ora_fine,max_iscritti) values ($1, $2, $3, $4, $5)',[gioco, data, ora_inizio, ora_fine, max_iscritti], function(error, results){
+        if(error){
+            errors.push({msg : "Errore di inserzione interno al database, contatta il tecnico se persiste"});
+            res.render('/adminDashboard', {
+                errors
+            });
+        }
+        req.flash('success_msg', 'Creazione andata a buon fine!');
+        res.redirect('../adminDashboard');
+    })
+});
 
 module.exports = router;
